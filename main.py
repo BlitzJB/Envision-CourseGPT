e719e9b4632511ebfc3b0759d2e6133908af12e9
BlitzJB <blitz04.dev@gmail.com>
2023-11-13 02:58:03 +0530

    open localhost:8000 automatically in browser

--- main.py
+++ main.py
@@ -66,4 +66,6 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
+    import webbrowser
+    webbrowser.open("http://localhost:8000")
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
